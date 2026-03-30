1901305e9cf20faee2fc45e8923149575cfcc887
Dezani <henrique.dezani@fatecriopreto.edu.br.local>
2026-03-30 18:16:04 -0300

    ActionResult, Views e Model

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -3,9 +3,17 @@ using Microsoft.AspNetCore.Mvc;
 
 public class TodoController : Controller
 {
-    // http://localhost:1234/todo/getnome
-    public string GetNome()
+    // atributo:
+    private List<string> tasks = new List<string>();
+    
+    // http://localhost:1234/todo/index
+    public ActionResult Index()
     {
-        return "Fulano";
+        tasks.Add("Estudar");
+        tasks.Add("Trabalhar");
+        tasks.Add("Dormir");
+
+        // /Views/Todo/Index.cshtml
+        return View(tasks);
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
